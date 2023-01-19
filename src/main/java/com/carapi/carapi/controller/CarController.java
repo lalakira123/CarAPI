9e8a951a64bee343dd84b07bca57091bc0676e0a
Laerte <laerteakira@gmail.com>
2023-01-19 19:26:06 -0300

    feat: Delete Method

--- src/main/java/com/carapi/carapi/controller/CarController.java
+++ src/main/java/com/carapi/carapi/controller/CarController.java
@@ -3,7 +3,9 @@ package com.carapi.carapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +32,11 @@ public class CarController {
   @PostMapping
   public void create(@RequestBody @Valid CarDTO req) {
     repository.save(new Car(req));
+  }
+
+  @DeleteMapping("/{id}")
+  public void delete(@PathVariable Long id) {
+    repository.deleteById(id);
   }
 
 }
